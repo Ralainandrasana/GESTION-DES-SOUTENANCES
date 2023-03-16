9768dfb37ed9cbf1098be96f13f2efa8c93f1bc5
Ralainandrasana <marsonmijoro@gmail.com>
2023-03-16 09:04:49 +0300

    send

---
+++ bouton.php
@@ -0,0 +1,10 @@
+<div class="bouton">
+    <form>
+        <input type="hidden">
+        <input type="submit" id="add" value="+">
+    </form>
+    <form>
+        <input type="text" id="input_search">
+        <input type="submit" id="search" value="search">
+    </form>
+</div>
